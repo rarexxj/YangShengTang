9b40ba1ec67125bfb9eb656a614b9a3e92d5408d
hujiaqi <2872265722@qq.com>
2016-10-27 15:54:41 +0800

    添加了客服电话接口

--- Modules/BntWeb.CustomerPhone/Content/Scripts/customerphone.list.js
+++ Modules/BntWeb.CustomerPhone/Content/Scripts/customerphone.list.js
@@ -1,1 +1,20 @@
-﻿
+﻿jQuery($(function () {
+    bntToolkit.initForm($("#VoucherForm"), {
+        CustomerPhone: {
+            required: true
+        }
+    }, beforeSubmit, success);
+}));
+
+function beforeSubmit(formData, jqForm, options) {
+    return true;
+}
+
+// post-submit callback
+function success(result, statusText, xhr, $form) {
+    if (!result.Success) {
+        bntToolkit.error(result.ErrorMessage);
+    } else {
+        location.href = url_list;
+    }
+}
